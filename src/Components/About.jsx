98dcfac2021e2e14c1958958ce4fede7bee7c1d8
Andrey <85174946+AndreyVaz@users.noreply.github.com>
2025-01-28 15:08:12 +0000

    changes with images and some infos.

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,15 +16,15 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/geometry.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "geometric objects and mathematical  on the chalkboard";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "Estudante de Análise e Desenvolvimento de Sistemas pela FAM. Gosto de Python e tenho interesse na área de Back-End.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "Now if pro is the opposite of con, what's the opposite of progress?.";
 
 const About = () => {
   return (
